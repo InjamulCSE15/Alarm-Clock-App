e6d8e5a54202e80c6451f402559ea135a4d6336d
unknown <yeasin.mohammod018@gmail.com>
2021-08-17 12:10:25 +0600

    CSS file done

---
+++ myScript.js
@@ -0,0 +1,37 @@
+
+var timer = document.getElementById('timer');
+var hours = document.getElementById('hours');
+var minutes = document.getElementById('minutes');
+var seconds = document.getElementById('seconds');
+var ampm = document.getElementById('ampm');
+var startStop = document.getElementById('startStop');
+
+var currentTime;
+var alarmElement;
+var activeAlarm = false;
+var alarmSound = new Audio("alarm_clock_ringtone.mp3");
+alarmSound.loop = true;
+
+// Show time in display:
+function showTime() {
+    var now = new Date();
+    currentTime = now.toLocaleTimeString();
+    timer.textContent = currentTime;
+    setTimeout(showTime, 1000);
+}
+
+showTime();
+
+// Add Time:
+
+function addMinSec(id) {
+    var select = id;
+    var min = 59;
+
+    for(i = 0; i <= min; i++) {
+        select.options[select.options.length] = new Option(i<10 ? "0" + i : i);
+    }
+}
+
+addMinSec(seconds);
+addMinSec(minutes);
